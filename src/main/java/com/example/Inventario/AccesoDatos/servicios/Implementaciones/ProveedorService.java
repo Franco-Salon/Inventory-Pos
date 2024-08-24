be546998f865f263520f5a45e54c7a79ac52cb54
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-23 18:24:56 -0600

    se agrego un metodo en los servicios de proveedor

--- src/main/java/com/example/Inventario/AccesoDatos/servicios/Implementaciones/ProveedorService.java
+++ src/main/java/com/example/Inventario/AccesoDatos/servicios/Implementaciones/ProveedorService.java
@@ -3,9 +3,12 @@ package com.example.Inventario.AccesoDatos.servicios.Implementaciones;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.example.Inventario.AccesoDatos.repositorios.IProveedorRepository;
 import com.example.Inventario.AccesoDatos.servicios.Interfaces.IProveedorService;
+import com.example.Inventario.EntidadesNegocio.Municipio;
 import com.example.Inventario.EntidadesNegocio.proveedor;
 
 @Service
@@ -14,9 +17,15 @@ public class ProveedorService implements IProveedorService{
     private IProveedorRepository proveedorRepository;
 
     @Override
-    public List<proveedor> obtenerTodos() {
+    public List<proveedor> listarTodos() {
         return proveedorRepository.findAll();
     }
+
+    @Override
+    public Page<proveedor> obtenerTodos(Pageable pageable) {
+        return proveedorRepository.findAll(pageable);
+    }
+
 
     @Override
     public Optional<proveedor> buscarPorId(Long id) {
--- src/main/java/com/example/Inventario/AccesoDatos/servicios/Interfaces/IProveedorService.java
+++ src/main/java/com/example/Inventario/AccesoDatos/servicios/Interfaces/IProveedorService.java
@@ -1,12 +1,17 @@
 package com.example.Inventario.AccesoDatos.servicios.Interfaces;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.Inventario.EntidadesNegocio.proveedor;
 
 public interface IProveedorService {
-    List<proveedor> obtenerTodos();
+
+     List<proveedor> listarTodos();
+
+    Page<proveedor> obtenerTodos(Pageable pageable);
 
     Optional<proveedor> buscarPorId(Long id);
 
