ac4954e4d07bf05ee567f4cbe02f244866a9cf70
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-23 18:21:01 -0600

    se modifico el metodo create en el controlador de empresa

--- src/main/java/com/example/Inventario/controller/EmpresaController.java
+++ src/main/java/com/example/Inventario/controller/EmpresaController.java
@@ -1,9 +1,14 @@
 package com.example.Inventario.controller;
 
 import com.example.Inventario.AccesoDatos.repositorios.IEmpresaRepository;
+import com.example.Inventario.AccesoDatos.servicios.Implementaciones.EmpresaService;
+import com.example.Inventario.AccesoDatos.servicios.Implementaciones.MunicipioService;
+import com.example.Inventario.AccesoDatos.servicios.Implementaciones.ProveedorService;
 import com.example.Inventario.AccesoDatos.servicios.Interfaces.IEmpresaService;
 import com.example.Inventario.EntidadesNegocio.Departamento;
 import com.example.Inventario.EntidadesNegocio.Empresa;
+import com.example.Inventario.EntidadesNegocio.proveedor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,22 +27,52 @@ import java.util.stream.IntStream;
 @Controller
 @RequestMapping("/empresas")
 public class EmpresaController {
+   
     @Autowired
-    private IEmpresaService empresaService;
+    private MunicipioService municipioService;
+
+    @Autowired
+    private EmpresaService empresaService;
+
+    @GetMapping
+    public String index(Model model, @RequestParam("page") Optional<Long> page, @RequestParam("size") Optional<Integer> size) {
+        long currentPage = page.orElse(1L) - 1L; // si no está seteado se asigna 0
+        int pageSize = size.orElse(10); // tamaño de la página, se asigna 5
+        Pageable pageable = PageRequest.of((int) currentPage, pageSize); // Se castea currentPage a int
+
+        Page<Empresa> empresas = empresaService.obtenerTodos(pageable);
+        model.addAttribute("empresas", empresas);
+
+        int totalPages = empresas.getTotalPages();
+        if (totalPages > 0) {
+            List<Long> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .mapToLong(i -> i) // Convertir a Long
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        return "empresa/index";
+    }
 
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("empresa", new Empresa());
+        model.addAttribute("municipios", municipioService.listarTodos());
         return "empresa/create";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute Empresa empresa, BindingResult result, RedirectAttributes attributes) {
-        if (result.hasErrors()) {
+    public String save(Empresa empresa, BindingResult result, Model model, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            model.addAttribute(empresa);
+            model.addAttribute("municipios", municipioService.listarTodos());
+            attributes.addFlashAttribute("error", "No se pudo guardar debido a un error.");
             return "empresa/create";
         }
+
         empresaService.crearOEditar(empresa);
-        attributes.addFlashAttribute("msg", "empresa guardado correctamente");
+        attributes.addFlashAttribute("msg", "Empresa guardada correctamente");
         return "redirect:/empresas";
     }
 
