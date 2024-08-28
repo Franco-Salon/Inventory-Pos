cfd30cdea797a9bde442b95b101d401c6a29ec2c
KevinSilva <kevinsilvadb503@gmail.com>
2024-08-28 10:35:50 -0600

    se arreglo itemproveedor por mala implementacion

--- src/main/java/com/example/Inventario/controller/ItemProveedorController.java
+++ src/main/java/com/example/Inventario/controller/ItemProveedorController.java
@@ -4,7 +4,11 @@ package com.example.Inventario.controller;
 import com.example.Inventario.AccesoDatos.servicios.Implementaciones.ItemProveedorService;
 import com.example.Inventario.AccesoDatos.servicios.Implementaciones.ItemService;
 import com.example.Inventario.AccesoDatos.servicios.Implementaciones.ProveedorService;
+import com.example.Inventario.AccesoDatos.servicios.Interfaces.IItemProveedorService;
+import com.example.Inventario.AccesoDatos.servicios.Interfaces.IItemService;
+import com.example.Inventario.AccesoDatos.servicios.Interfaces.IProveedorService;
 import com.example.Inventario.EntidadesNegocio.ItemProveedor;
+import com.example.Inventario.EntidadesNegocio.Rol;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,122 +21,93 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/itemproveedor")
+@RequestMapping("/itemproveedores")
 public class ItemProveedorController
 {
-    // Representan La DB:
     @Autowired
-    private ItemProveedorService _ItemProveedorService;
-
-    @Autowired
-    private ItemService _ItemService;
+    private IItemProveedorService ItemProveedorService;
 
     @Autowired
-    private ProveedorService _ProveedorService;
+    private IItemService ItemService;
+
+    @Autowired
+    private IProveedorService ProveedorService;
 
 
 
-    // OBTIENE TODOS LOS REGISTROS Y LOS MANDA A LA VISTA:
-    @GetMapping("/index")
+    @GetMapping
     public String index(Model model)
     {
-        model.addAttribute("Lista_ItemProveedores", _ItemProveedorService.obtenerTodos());
-
-        return "ItemProveedor/Index";
+        model.addAttribute("itemproveedor", ItemProveedorService.obtenerTodos());
+        return "itemproveedor/index";
     }
 
-    // NOS MANDA A LA VISTA:
+
     @GetMapping("/create")
     public String create(ItemProveedor itemProveedor, Model model)
     {
-        // Registros De Item:
-        model.addAttribute("Lista_Item", _ItemService.obtenerTodos());
-
-        // Registros De Proveedor:
-        model.addAttribute("Lista_Proveedor", _ProveedorService.listarTodos());
-
-        // Objeto Vasio:
-        model.addAttribute("itemProveedor", itemProveedor);
+        model.addAttribute("item", ItemService.obtenerTodos());
+        model.addAttribute("proveedor", ProveedorService.listarTodos());
+        model.addAttribute("itemproveedor", itemProveedor);
 
 
-        return "ItemProveedor/Create";
+        return "Itemproveedor/Create";
     }
 
 
-    // RECIBE UN OBJETO Y LO GUARDA EN LA DB:
+
     @PostMapping("/save")
     public String save(@Valid ItemProveedor itemProveedor, BindingResult result, Model model, RedirectAttributes attributes)
     {
-        // Validar Los Campos:
-        if (result.hasErrors())
-        {
-            // Registros De Item:
-            model.addAttribute("Lista_Item", _ItemService.obtenerTodos());
-
-            // Registros De Proveedor:
-            model.addAttribute("Lista_Proveedor", _ProveedorService.listarTodos());
-
-            model.addAttribute("itemProveedor", itemProveedor);
-
-            return "ItemProveedor/Create";
-        }
-
-        // Guardando
-        _ItemProveedorService.crearOEditar(itemProveedor);
-        attributes.addFlashAttribute("msg", "Usuario guardado correctamente");
-
-        return "redirect:/itemproveedor/index";
+        ItemProveedorService.crearOEditar(itemProveedor);
+        attributes.addFlashAttribute("msg", "Item proveedor guardado correctamente");
+        return "redirect:/itemproveedores/index";
     }
 
-    // Busca Un Registro Con El Mismo ID en la DB:
+
     @GetMapping("/details/{id}")
     public String details(@PathVariable("id") Integer id, Model model)
     {
-        model.addAttribute("Objeto_Obtenido", _ItemProveedorService.buscarPorId(id).get());
-
-        return "ItemProveedor/Details";
+        model.addAttribute("itemproveedor", ItemProveedorService.buscarPorId(id).get());
+        return "itemproveedor/details";
     }
 
 
-    // Busca Un Registro Con El Mismo Id Y Nos Manda A La Vista:
+
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable("id") Integer id, Model model)
     {
-        // Registro Encontrado:
-        model.addAttribute("Objeto_Obtenido", _ItemProveedorService.buscarPorId(id).get());
-
-        // Registros De Item:
-        model.addAttribute("Lista_Item", _ItemService.obtenerTodos());
-
-        // Registros De Proveedor:
-        model.addAttribute("Lista_Proveedor", _ProveedorService.listarTodos());
-
-
-        return "/ItemProveedor/Edit";
+        
+        model.addAttribute("itemproveedor", ItemProveedorService.buscarPorId(id).get());
+        model.addAttribute("item", ItemService.obtenerTodos());
+        model.addAttribute("proveedor", ProveedorService.listarTodos());
+        return "/itemproveedor/edit";
     }
 
-
-    // Nos Manda A La Vista Eliminar Con Un Registro Encontrado
     @GetMapping("/remove/{id}")
     public String remove(@PathVariable("id") Integer id, Model model)
     {
-        // Registro Encontrado:
-        model.addAttribute("Objeto_Obtenido", _ItemProveedorService.buscarPorId(id).get());
-
-        return "ItemProveedor/Delete";
+        Optional<ItemProveedor> itemProveedor = ItemProveedorService.buscarPorId(id);
+        if (itemProveedor.isPresent()) {
+            model.addAttribute("rol", itemProveedor.get());
+            return "itemproveedor/delete";
+        } else {
+            return "redirect:/itemproveedores";
+        }
     }
 
 
-    // Busca El Registro Y Lo Elimina De La DB:
+
     @PostMapping("/delete")
     public String delete(ItemProveedor itemProveedor, RedirectAttributes attributes)
     {
-        _ItemProveedorService.eliminarPorId(itemProveedor.getId());
+        ItemProveedorService.eliminarPorId(itemProveedor.getId());
         attributes.addFlashAttribute("msg", "ItemProveedor Eliminado correctamente");
 
-        return "redirect:/itemproveedor/index";
+        return "redirect:/itemproveedores";
     }
 
 
